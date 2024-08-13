dbc4e37320c30f87504be98510dd654be1c29e09
Rinze-CodeGorilla <132885446+Rinze-CodeGorilla@users.noreply.github.com>
2024-08-13 16:54:45 +0200

    Initial commit

---
+++ src/main/java/nl/codegorilla/jmsclientdemo/JmsClientDemoApplication.java
@@ -0,0 +1,13 @@
+package nl.codegorilla.jmsclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsClientDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsClientDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/codegorilla/jmsclientdemo/JmsClientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.codegorilla.jmsclientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsClientDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
